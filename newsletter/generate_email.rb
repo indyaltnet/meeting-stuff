958c298c8a8a204b49559adb525d50085cf159db
Jon Fuller <fullerjc@gmail.com>
2010-04-14 15:13:10 -0400

    add newsletter template stuff

---
+++ newsletter/generate_email.rb
@@ -0,0 +1,49 @@
+require 'erb'
+require 'yamlconfig.rb'
+require 'active_support'
+
+class Newsletter
+  include YAMLConfig
+
+  attr_accessor :month
+  attr_accessor :year
+
+  def write()
+    template = <<END_EMAIL
+***** Indy ALT.NET - <%= "#{@month} #{@year}" %> Newsletter *****
+
+** <%= @month %> Meeting Info **
+- Location: <%= @month %>
+- Date: Thursday, <%= "#{@month}, #{@day}  #{@year}" %> 
+- Time: Formal discussion begins at 6pm, food begins at 5:30pm, networking begins at 5:15pm.
+- Topic: <%= @topic %>
+- Speakers: <%= @speaker %>
+- More information:
+<%= @meeting_link %>
+
+<%= @synopsis %>
+
+** Food **
+Pizza and networking will begin around 5:15 with the technology discussion beginning at 6:00.
+
+** Location **
+<%= @location_long %>
+
+** Interesting Blog Posts and Other Links **
+<% @links.each do |link| %>
+
+<%= link["title"] %>
+
+<%= link["link"] %>
+
+<% end %>
+END_EMAIL
+
+    erb = ERB.new(template, 0, "%<>")
+    puts erb.result(binding) 
+  end
+end
+
+n = Newsletter.new
+n.configure('april.yml')
+n.write()
